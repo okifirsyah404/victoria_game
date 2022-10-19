fc1f3bfdc16f6acb689ea04572c760de07e49e97
okifirsyah404 <okifirsyah404@gmail.com>
2022-10-19 14:11:15 +0700

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:victoria_game/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    // await tester.pumpWidget(const GetMat());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
