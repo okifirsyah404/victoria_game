8b2afa7006871c33f49dd069cf1f1d7e443a0307
okifirsyah404 <okifirsyah404@gmail.com>
2022-11-10 05:01:08 +0700

    Update development

--- lib/app/modules/profile_settings/user_profile/controllers/profile_settings_user_profile_controller.dart
+++ lib/app/modules/profile_settings/user_profile/controllers/profile_settings_user_profile_controller.dart
@@ -1,7 +1,17 @@
 import 'package:get/get.dart';
 
+import '../../../../core/services/firebase_auth_services.dart';
+import '../../../../routes/app_pages.dart';
+
 class ProfileSettingsUserProfileController extends GetxController {
   //TODO: Implement ProfileSettingsUserProfileController
+
+  FirebaseAuthServices firebaseAuthServices = FirebaseAuthServices();
+
+  void signOut() {
+    firebaseAuthServices.signOutUserPasswordBased();
+    Get.offAllNamed(Routes.AUTH_SIGN_IN);
+  }
 
   @override
   void onInit() async {
--- lib/app/modules/profile_settings/user_profile/views/profile_settings_user_profile_view.dart
+++ lib/app/modules/profile_settings/user_profile/views/profile_settings_user_profile_view.dart
@@ -244,6 +244,9 @@ class ProfileSettingsUserProfileView
             DividerListTile(
               title: "Keluar",
               bottomBorder: true,
+              onTap: () {
+                controller.signOut();
+              },
             ),
           ],
         ),
